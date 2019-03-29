2cbf191a24235712422841be535b61025b159882
eragueneau <159753.AZErty>
2019-03-29 18:20:43 +0100

    a et b faits

--- Menu.pl
+++ Menu.pl
@@ -6,39 +6,71 @@ use DBI;
 my $dbh = DBI->connect("DBI:Pg:dbname=eragueneau;host=dbserver", "eragueneau", "motdepasse", { RaiseError => 0 });
 print "Menu\n";
 
-print "1. Ajouter_prot\n";
-print "2. Modifier_séquence\n";
-print "3. Afficher_nom_protéines\n";
-print "4. Afficher_nom_gènes\n";
-print "5. Afficher_prot_selon_Usr\n";
-print "6. Afficher_caractéristiques_protéines\n";
+print "1. Ajouter prot\n";
+print "2. Modifier séquence\n";
+print "3. Afficher nom protéines\n";
+print "4. Afficher nom gènes\n";
+print "5. Afficher prot selon Usr\n";
+print "6. Afficher caractéristiques protéines\n";
 
-while (my $action = <>) {
+my $action = <STDIN>;
+while ($action != 0) {
     if ($action == 1) {
-        #print ("Nom de la protéine ")
-        my $entry = <>;
-        my $name = <>;
-        my $sequence = <>;
+        print("Entrée de la protéine (code Uniprot) ?\n");
+        my $entry = <STDIN>;
+        chomp $entry;
+        print("Nom de la protéine ?\n");
+        my $name = <STDIN>;
+        chomp $name;
+        print("Séquence de la protéine ?\n");
+        my $sequence = <STDIN>;
+        chomp $sequence;
+        $sequence = uc $sequence;
+        while ($sequence =~ /[^AC-NP-TVWY]/) {
+            print("Séquence de la protéine invalide, veuillez la modifier.\n");
+            $sequence = <STDIN>;
+            chomp $sequence;
+            $sequence = uc $sequence;
+        }
         my $longueur = length($sequence);
-        $dbh->do("INSERT INTO Proteins VALUES ($entry, $name, $sequence, $length);")
+        my $add_prot = $dbh->prepare("INSERT INTO Proteins VALUES (?, ?, ?, ?);");
+        $add_prot->execute($entry, $name, $sequence, $longueur);
+        print("$entry, $name, $sequence, $longueur \n");
+    }
+    if ($action == 2) {
+        print "Entrez l'entrée de la protéine à modifier.\n";
+        my $entry = <STDIN>;
+        chomp $entry;
+        print "Quelle est la nouvelle séquence ?\n";
+        my $sequence_alt = <STDIN>;
+        chomp $sequence_alt;
+        $sequence_alt = uc $sequence_alt;
+        while ($sequence_alt =~ /[^AC-NP-TVWY]/) {
+            print("Séquence invalide, veuillez la modifier.\n");
+            $sequence_alt = <STDIN>;
+            chomp $sequence_alt;
+            $sequence_alt = uc $sequence_alt;
+        }
+
+        my $longueur = length($sequence_alt);
+
+        my $mod_seq = $dbh->prepare("UPDATE Proteins SET sequence = ?, length = ? where entry = ?;");
+
+        $mod_seq->execute($sequence_alt, $longueur, $entry);
+
+    }
+
+    if ($action == 3) {
 
 
     }
-    if ($action==2){
-        print"entrz ID du gene cible\n";
-        my $entry = <>;
-        #my $name = <>;
-        print"seq_alt\n";
-        my $sequence_alt=<>;
-        my $longueur = length($sequence_alt);}
-
-        $dbh->do("UPDATE Proteins SET sequence = $sequence_alt, length = $longueur where entry = $entry;");
-
-    if ($action==3){
-
-
-
-    }
-    }
+    print "1. Ajouter une protéine\n";
+    print "2. Modifier une séquence protéique\n";
+    print "3. Afficher nom protéines\n";
+    print "4. Afficher nom gènes\n";
+    print "5. Afficher prot selon Usr\n";
+    print "6. Afficher caractéristiques protéines\n";
+    $action = <STDIN>;
+}
 
 
--- create_db.pl
+++ create_db.pl
@@ -5,78 +5,72 @@ use DBI;
 
 my $dbh = DBI->connect("DBI:Pg:dbname=eragueneau;host=dbserver", "eragueneau", "motdepasse", { RaiseError => 0 });
 
-# $dbh->do("DROP TABLE Reactions;");
-# $dbh->do("DROP TABLE Genes;");
-# $dbh->do("DROP TABLE Metadata;");
-# $dbh->do("DROP TABLE Proteins;");
+$dbh->do("DROP TABLE Reactions;");
+$dbh->do("DROP TABLE Genes;");
+$dbh->do("DROP TABLE Metadata;");
+$dbh->do("DROP TABLE Proteins;");
 
-# $dbh->do("CREATE TABLE Proteins(
-#     Entry VARCHAR(10) CONSTRAINT clef_proteines PRIMARY KEY,
-#     Names VARCHAR(800),
-#     Sequence VARCHAR(100000) CONSTRAINT sequence_error CHECK(Sequence SIMILAR TO '[ACDEFGHIKLMNPQRSTVWY]*'),
-#     Length INT CONSTRAINT positive_length CHECK(Length>0)
-# );");
-#
-# $dbh->do("CREATE TABLE Reactions(
-#     Entry VARCHAR(10) CONSTRAINT clef_reactions PRIMARY KEY REFERENCES Proteins(Entry),
-#     Transcript_ID VARCHAR(20) CONSTRAINT error_transcript CHECK(Transcript_ID SIMILAR TO 'AT[0-9]G[0-9]+.[0-9]'),
-#     Plant_Reaction VARCHAR(20) CONSTRAINT error_reaction CHECK(Plant_Reaction SIMILAR TO 'R-ATH-[0-9]+' OR NULL)
-#
-# );");
-#
-# $dbh->do("CREATE TABLE Genes(
-#     Entry VARCHAR(10) CONSTRAINT clef_genes PRIMARY KEY REFERENCES Proteins(Entry),
-#     Names VARCHAR(1000),
-#     Ontology VARCHAR(10000) CONSTRAINT need_go_id CHECK(Ontology SIMILAR TO 'GO:\[0-9]+' ESCAPE '!' OR NULL),
-#     Synonyme VARCHAR(1000)
-#
-# );");
-#
-# $dbh->do("CREATE TABLE Metadata(
-#     Entry VARCHAR(10) CONSTRAINT clef_meta PRIMARY KEY REFERENCES Proteins(Entry),
-#     Status VARCHAR(20) CONSTRAINT reviewed_unreviewed CHECK(Status in ('reviewed','unreviewed')),
-#     Organism VARCHAR(100) CONSTRAINT arath_only CHECK(Organism='Arabidopsis thaliana (Mouse-ear cress)'),
-#     Entry_names VARCHAR(50) CONSTRAINT arath_end CHECK(Entry_names SIMILAR TO '[A-Z0-9]+_ARATH')
-# );");
-#
-# open(my $file, "uniprot-arabidopsisthalianaSequence.tab") || die "can't open uniprot_data";
-#
-# my $protein_insert = $dbh->prepare("INSERT INTO proteins VALUES (?, ?, ?, ?)");
-# my $gene_insert = $dbh->prepare("INSERT INTO genes VALUES (?, ?, ?, ?)");
-# my $meta_data_insert = $dbh->prepare("INSERT INTO metadata VALUES (?, ?, ?, ?)");
-#
-# my $header = <$file>;
-# while (my $line = <$file>) {
-#     chomp $line;
-#     my @tab = split(/\t/, $line);
-#     if ($tab[5] eq "Arabidopsis thaliana (Mouse-ear cress)") {
-#         $protein_insert->execute($tab[0], $tab[3], $tab[10], $tab[6]);
-#         $gene_insert->execute($tab[0], $tab[4], $tab[8], $tab[7]);
-#         $meta_data_insert->execute($tab[0], $tab[2], $tab[5], $tab[1]);
-#     }
-# }
-# close($file);
-#
-# open($file, "mart_export.csv") || die "can't open EnsemblPlants data";
-#
-# my $reactoins_insert = $dbh->prepare("INSERT INTO reactions VALUES (?,?,?)");
-#
-# $header = <$file>;
-# while (my $line = <$file>) {
-#     chomp $line;
-#     my @tab = split(/,/, $line);
-#     if (!defined($tab[3])) {
-#         $tab[3] = 'NULL'
-#     }
-#     if (!defined($tab[1])) {
-#         $tab[1] = 'NULL'
-#     }
-#     $reactoins_insert->execute($tab[2], $tab[1], $tab[3])
-# }
+$dbh->do("CREATE TABLE Proteins(
+    Entry VARCHAR(10) CONSTRAINT clef_proteines PRIMARY KEY,
+    Names VARCHAR(800),
+    Sequence VARCHAR(100000) CONSTRAINT sequence_error CHECK(Sequence SIMILAR TO '[ACDEFGHIKLMNPQRSTVWY]*'),
+    Length INT CONSTRAINT positive_length CHECK(Length>0)
+);");
 
-my $request = $dbh->prepare("select * from proteins where names SIMILAR TO ?;");
-$request->execute("%EC 4.1.1.65%");
-while (my @t = $request->fetchrow_array()) {
-    print join(" ", @t), "\n";
+$dbh->do("CREATE TABLE Reactions(
+    Entry VARCHAR(10) CONSTRAINT clef_reactions PRIMARY KEY REFERENCES Proteins(Entry),
+    Transcript_ID VARCHAR(20) CONSTRAINT error_transcript CHECK(Transcript_ID SIMILAR TO 'AT[0-9]G[0-9]+.[0-9]'),
+    Plant_Reaction VARCHAR(20) CONSTRAINT error_reaction CHECK(Plant_Reaction SIMILAR TO 'R-ATH-[0-9]+' OR NULL)
+
+);");
+
+$dbh->do("CREATE TABLE Genes(
+    Entry VARCHAR(10) CONSTRAINT clef_genes PRIMARY KEY REFERENCES Proteins(Entry),
+    Names VARCHAR(1000),
+    Ontology VARCHAR(10000) CONSTRAINT need_go_id CHECK(Ontology SIMILAR TO 'GO:\[0-9]+' ESCAPE '!' OR NULL),
+    Synonyme VARCHAR(1000)
+
+);");
+
+$dbh->do("CREATE TABLE Metadata(
+    Entry VARCHAR(10) CONSTRAINT clef_meta PRIMARY KEY REFERENCES Proteins(Entry),
+    Status VARCHAR(20) CONSTRAINT reviewed_unreviewed CHECK(Status in ('reviewed','unreviewed')),
+    Organism VARCHAR(100) CONSTRAINT arath_only CHECK(Organism='Arabidopsis thaliana (Mouse-ear cress)'),
+    Entry_names VARCHAR(50) CONSTRAINT arath_end CHECK(Entry_names SIMILAR TO '[A-Z0-9]+_ARATH')
+);");
+
+open(my $file, "uniprot-arabidopsisthalianaSequence.tab") || die "can't open uniprot_data";
+
+my $protein_insert = $dbh->prepare("INSERT INTO proteins VALUES (?, ?, ?, ?)");
+my $gene_insert = $dbh->prepare("INSERT INTO genes VALUES (?, ?, ?, ?)");
+my $meta_data_insert = $dbh->prepare("INSERT INTO metadata VALUES (?, ?, ?, ?)");
+
+my $header = <$file>;
+while (my $line = <$file>) {
+    chomp $line;
+    my @tab = split(/\t/, $line);
+    if ($tab[5] eq "Arabidopsis thaliana (Mouse-ear cress)") {
+        $protein_insert->execute($tab[0], $tab[3], $tab[10], $tab[6]);
+        $gene_insert->execute($tab[0], $tab[4], $tab[8], $tab[7]);
+        $meta_data_insert->execute($tab[0], $tab[2], $tab[5], $tab[1]);
+    }
 }
+close($file);
+
+open($file, "mart_export.csv") || die "can't open EnsemblPlants data";
+
+my $reactoins_insert = $dbh->prepare("INSERT INTO reactions VALUES (?,?,?)");
+
+$header = <$file>;
+while (my $line = <$file>) {
+    chomp $line;
+    my @tab = split(/,/, $line);
+    $reactoins_insert->execute($tab[2], $tab[1], $tab[3])
+}
+
+# my $request = $dbh->prepare("select * from proteins where names SIMILAR TO ?;");
+# $request->execute("%EC 4.1.1.65%");
+# while (my @t = $request->fetchrow_array()) {
+#     print join(" ", @t), "\n";
+# }
 
