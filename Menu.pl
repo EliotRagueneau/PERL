118fec58108d25eec1763e387b87da950e91cb5c
eragueneau <159753.AZErty>
2019-04-02 13:34:03 +0200

    correction EC Number accept no link between protein and ensemblPlant + Refactoring

--- Menu.pl
+++ Menu.pl
@@ -5,8 +5,44 @@ use DBI;
 
 my $dbh = DBI->connect("DBI:Pg:dbname=eragueneau;host=dbserver", "eragueneau", "motdepasse", { RaiseError => 0 });
 
+sub menu {
+    print "\n\n\tMenu\n";
+    print ">1 Ajouter une protéine\n";
+    print ">2 Modifier une séquence protéique\n";
+    print ">3 Afficher le nom des protéines (identifiant UniProt) qui sont référencées dans la table issue de EnsemblPlant\n";
+    print ">4 Afficher le nom des gènes du fichier UniProt qui sont également référencés dans le fichier EnsemblPlant\n";
+    print ">5 Afficher les protéines d'une taille supérieure à ...\n";
+    print ">6 Afficher les caractéristiques dune protéine selon son identifiant E.C.\n";
+    print ">0 Quitter\n";
+}
+
+sub get_sequence {
+    my $sequence = <STDIN>;
+    chomp $sequence;
+    $sequence = uc $sequence;
+    if ($sequence =~ /[^A-IK-NP-TV-Z]/) {
+        print("Séquence protéique invalide, veuillez la modifier.\n");
+        $sequence = get_sequence();
+    }
+    return $sequence;
+}
+
+sub save_yes_no {
+    print "Voulez vous sauvegarder les résultats [O|N]\n";
+    my $answer = <STDIN>;
+    chomp $answer;
+    return uc $answer;
+}
+
+sub show_results {
+    my $executed_querry = shift;
+    while (my @t = $executed_querry->fetchrow_array()) {
+        print join(" | ", @t), "\n"
+    }
+}
+
 sub save {
-    my $request = shift;
+    my $executed_querry = shift;
     my $file_name = shift;
     my $title = shift;
     my @column_names = @_;
@@ -17,7 +53,7 @@ sub save {
     print $fh "<table>\n\t<thead>\n\t\t<tr>\n\t\t\t<th>";
     print $fh join("</th>\n\t\t\t<th>", @column_names);
     print $fh "</th>\n\t\t</tr>\n\t</thead>\n\t<tbody>\n";
-    while (my @t = $request->fetchrow_array()) {
+    while (my @t = $executed_querry->fetchrow_array()) {
         for (@t) {$_ = 'N.A.' if !defined($_);}
         print $fh "\t\t<tr>\n\t\t\t<td>";
         print $fh join("</td>\n\t\t\t<td>", @t);
@@ -27,38 +63,24 @@ sub save {
     close($fh);
 }
 
-sub menu {
-    print "Menu\n";
-    print "-1. Ajouter une protéine\n";
-    print "-2. Modifier une séquence protéique\n";
-    print "-3. Afficher nom des protéines dont l'entrée est présente dans EnsemblPlant\n";
-    print "-4. Afficher nom gènes dont l'entrée est présente dans EnsemblPlant\n";
-    print "-5. Afficher les protéines d'une taille supérieure à ...\n";
-    print "-6. Afficher les caractéristiques dune protéine selon son identifant E.C.\n";
-    print "-0. Quitter\n";
-}
-
 menu;
 my $action = <STDIN>;
+
 while ($action != 0) {
     if ($action == 1) {
         print("Entrée de la protéine (code Uniprot) ?\n");
         my $entry = <STDIN>;
         chomp $entry;
+
         print("Nom de la protéine ?\n");
         my $name = <STDIN>;
         chomp $name;
+
         print("Séquence de la protéine ?\n");
-        my $sequence = <STDIN>;
-        chomp $sequence;
-        $sequence = uc $sequence;
-        while ($sequence =~ /[^AC-IK-NP-TV-Z]/) {
-            print("Séquence de la protéine invalide, veuillez la modifier.\n");
-            $sequence = <STDIN>;
-            chomp $sequence;
-            $sequence = uc $sequence;
-        }
+        my $sequence = get_sequence();
+
         my $longueur = length($sequence);
+
         my $add_prot = $dbh->prepare("INSERT INTO Proteins VALUES (?, ?, ?, ?);");
         $add_prot->execute($entry, $name, $sequence, $longueur);
         print("$entry, $name, $sequence, $longueur \n");
@@ -68,34 +90,26 @@ while ($action != 0) {
         print "Entrez l'entrée de la protéine à modifier.\n";
         my $entry = <STDIN>;
         chomp $entry;
-        print "Quelle est la nouvelle séquence ?\n";
-        my $sequence_alt = <STDIN>;
-        chomp $sequence_alt;
-        $sequence_alt = uc $sequence_alt;
-        while ($sequence_alt =~ /[^AC-IK-NP-TV-Z]/) {
-            print("Séquence invalide, veuillez la modifier.\n");
-            $sequence_alt = <STDIN>;
-            chomp $sequence_alt;
-            $sequence_alt = uc $sequence_alt;
-        }
 
-        my $longueur = length($sequence_alt);
+        print "Quelle est la nouvelle séquence ?\n";
+        my $sequence = get_sequence();
+
+        my $longueur = length($sequence);
 
         my $mod_seq = $dbh->prepare("UPDATE Proteins SET sequence = ?, length = ? where entry = ?;");
-
-        $mod_seq->execute($sequence_alt, $longueur, $entry);
+        my $nb_modified = $mod_seq->execute($sequence, $longueur, $entry);
+        if ($nb_modified == 0) {
+            print("L'entrée donnée n'éxistant pas dans la base, nous n'avons pas pu modifier sa séquence\n");
+        }
     }
 
     if ($action == 3) {
         my $name_prot = $dbh->prepare("SELECT p.entry, names FROM proteins p JOIN reactions r ON p.entry = r.entry;");
         $name_prot->execute();
-        while (my @t = $name_prot->fetchrow_array()) {
-            print join(" |\t", @t), "\n"
-        }
-        print "Voulez vous sauvegarder les résultats [O|N]\n";
-        my $answer = <STDIN>;
-        chomp $answer;
-        $answer = uc $answer;
+
+        show_results($name_prot);
+
+        my $answer = save_yes_no();
         if ($answer eq "O") {
             $name_prot->execute();
             save($name_prot, "linked_protein_names", "Proteins linked with EnsemblPlants", "Entry", "Protein name");
@@ -106,13 +120,10 @@ while ($action != 0) {
     if ($action == 4) {
         my $name_gene = $dbh->prepare("SELECT g.entry, names FROM genes g JOIN reactions r ON g.entry = r.entry WHERE names IS NOT NULL;");
         $name_gene->execute();
-        while (my @t = $name_gene->fetchrow_array()) {
-            print join(" |\t", @t), "\n"
-        }
-        print "Voulez vous sauvegarder les résultats [O|N]\n";
-        my $answer = <STDIN>;
-        chomp $answer;
-        $answer = uc $answer;
+
+        show_results($name_gene);
+
+        my $answer = save_yes_no();
         if ($answer eq "O") {
             $name_gene->execute();
             save($name_gene, "linked_gene_names", "Genes linked with EnsemblPlants", "Entry", "Gene names");
@@ -124,15 +135,13 @@ while ($action != 0) {
         print("Les protéines affichées doivent avoir une taille supérieure à : ");
         my $size = <STDIN>;
         chomp $size;
+
         my $prot_sup = $dbh->prepare("SELECT entry, names, length FROM proteins WHERE length >= ? ORDER BY length DESC;");
         $prot_sup->execute($size);
-        while (my @t = $prot_sup->fetchrow_array()) {
-            print join(" |\t", @t), "\n";
-        }
-        print "Voulez vous sauvegarder les résultats [O|N]\n";
-        my $answer = <STDIN>;
-        chomp $answer;
-        $answer = uc $answer;
+
+        show_results($prot_sup);
+
+        my $answer = save_yes_no();
         if ($answer eq "O") {
             $prot_sup->execute();
             my $name = "prot_sup_" . $size;
@@ -151,22 +160,28 @@ while ($action != 0) {
             $ec_id = <STDIN>;
             chomp $ec_id;
         }
+
         my $ec = "%EC " . $ec_id . "%";
+
         my $ec_car = $dbh->prepare("
-        SELECT m.entry, m.entry_names, m.status ,p.names, p.sequence, p.length, r.transcript_id, r.plant_reaction
+        SELECT p.entry, m.entry_names, m.status ,p.names, p.sequence, p.length, r.transcript_id, r.plant_reaction
         FROM proteins p
             JOIN reactions r ON p.entry = r.entry
             JOIN metadata m ON p.entry = m.entry
-        WHERE p.names SIMILAR TO ?;");
+        WHERE p.names LIKE ?;");
 
-        $ec_car->execute($ec);
-        while (my @t = $ec_car->fetchrow_array()) {
-            print join(" |\t", @t), "\n";
+        if ($ec_car->execute($ec) == 0) {
+            $ec_car = $dbh->prepare("
+        SELECT p.entry, m.entry_names, m.status ,p.names, p.sequence, p.length
+        FROM proteins p
+            JOIN metadata m ON p.entry = m.entry
+        WHERE p.names LIKE ?;");
+            $ec_car->execute($ec)
         }
-        print "Voulez vous sauvegarder les résultats [O|N]\n";
-        my $answer = <STDIN>;
-        chomp $answer;
-        $answer = uc $answer;
+
+        show_results($ec_car);
+
+        my $answer = save_yes_no();
         if ($answer eq "O") {
             $ec_car->execute();
             my $name = "carac_EC_" . $ec_id;
@@ -178,7 +193,6 @@ while ($action != 0) {
 
     menu;
     $action = <STDIN>;
-
 }
 
 
--- create_db.pl
+++ create_db.pl
@@ -26,7 +26,7 @@ $dbh->do("CREATE TABLE Reactions(
 $dbh->do("CREATE TABLE Genes(
     Entry VARCHAR(10) CONSTRAINT clef_genes PRIMARY KEY REFERENCES Proteins(Entry),
     Names VARCHAR(1000),
-    Ontology VARCHAR(10000) CONSTRAINT need_go_id CHECK(Ontology SIMILAR TO 'GO:\[0-9]+' ESCAPE '!' OR NULL),
+    Ontology VARCHAR(10000) CONSTRAINT need_go_id CHECK(Ontology SIMILAR TO 'GO:\[0-9]+' OR NULL),
     Synonyme VARCHAR(1000)
 
 );");
@@ -38,16 +38,16 @@ $dbh->do("CREATE TABLE Metadata(
     Entry_names VARCHAR(50) CONSTRAINT arath_end CHECK(Entry_names SIMILAR TO '[A-Z0-9]+_ARATH')
 );");
 
-open(my $file, "uniprot-arabidopsisthalianaSequence.tab") || die "can't open uniprot_data";
+open(UNIPROT, "uniprot-arabidopsisthalianaSequence.tab") || die "can't open uniprot_data";
 
 my $protein_insert = $dbh->prepare("INSERT INTO proteins VALUES (?, ?, ?, ?)");
 my $gene_insert = $dbh->prepare("INSERT INTO genes VALUES (?, ?, ?, ?)");
 my $meta_data_insert = $dbh->prepare("INSERT INTO metadata VALUES (?, ?, ?, ?)");
 
-my $header = <$file>;
-while (my $line = <$file>) {
-    chomp $line;
-    my @tab = split(/\t/, $line);
+<UNIPROT>;
+while (<UNIPROT>) {
+    chomp;
+    my @tab = split(/\t/, $_);
     if ($tab[5] eq "Arabidopsis thaliana (Mouse-ear cress)") {
         $protein_insert->execute($tab[0], $tab[3], $tab[10], $tab[6]);
         if ($tab[4] eq "") {
@@ -57,16 +57,16 @@ while (my $line = <$file>) {
         $meta_data_insert->execute($tab[0], $tab[2], $tab[5], $tab[1]);
     }
 }
-close($file);
+close(UNIPROT);
 
-open($file, "mart_export.csv") || die "can't open EnsemblPlants data";
+open(ENSEMBL, "mart_export.csv") || die "can't open EnsemblPlants data";
 
 my $reactoins_insert = $dbh->prepare("INSERT INTO reactions VALUES (?,?,?)");
 
-$header = <$file>;
-while (my $line = <$file>) {
-    chomp $line;
-    my @tab = split(/,/, $line);
+<ENSEMBL>;
+while (<ENSEMBL>) {
+    chomp;
+    my @tab = split(/,/, $_);
     $reactoins_insert->execute($tab[2], $tab[1], $tab[3])
 }
-
+close(ENSEMBL)
